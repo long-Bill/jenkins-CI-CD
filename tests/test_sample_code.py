070f793945b719b370e1cd7ec8b8610b278a80b6
long-Bill <telleph0ne34@gmail.com>
2025-11-11 09:06:33 +0000

    comment

--- tests/test_sample_code.py
+++ tests/test_sample_code.py
@@ -15,9 +15,9 @@ def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(-2, 2) == -4
 
-#Testtttttttt
+
 def test_divide():
-    assert divide(10, 2) == 5
+    #assert divide(10, 2) == 5
     assert divide(9, 3) == 3
 
     with pytest.raises(ValueError):
