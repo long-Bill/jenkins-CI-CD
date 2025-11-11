e4782b3a5ee14885541ae44132cc75c4ffaced5d
long-Bill <telleph0ne34@gmail.com>
2025-11-11 07:34:03 +0000

    test

--- tests/test_sample_code.py
+++ tests/test_sample_code.py
@@ -5,6 +5,7 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(1.5, 1.5) == 3.0
+    assert add(1,1) == 2
 
 def test_subtract():
     assert subtract(10, 5) == 5
